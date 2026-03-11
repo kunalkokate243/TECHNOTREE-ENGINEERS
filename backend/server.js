a918c226c1f4d099c86685ac1da4796fbcd438ec
iamgautam5061 <iamgautam9363@gmail.com>
2026-03-11 14:51:36 +0530

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const contactRoutes = require("./routes/contactRoutes");
 
@@ -9,7 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/technotree");
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI);
 
 mongoose.connection.on("connected", () => {
   console.log("MongoDB connected");
@@ -21,6 +23,6 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
